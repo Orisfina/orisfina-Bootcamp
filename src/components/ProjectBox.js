f07076df751d518141a6a9557f75552ff0650acc
Joshua Oyewole <joshpp2013@gmail.com>
2021-10-29 21:37:47 +0100

    start replaced with flex-start

--- src/components/ProjectBox.js
+++ src/components/ProjectBox.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import {
+/* import {
      BrowserRouter as Router,
      Link
-   } from "react-router-dom";
+   } from "react-router-dom"; */
 
 function ProjectBox({projects}) {
      
