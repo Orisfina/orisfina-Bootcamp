4ce5673e05a06ae34e3d1c0f12713cb69c494a4b
Joshua Oyewole <joshpp2013@gmail.com>
2021-11-01 13:34:23 +0100

    unused variables removed

--- src/components/ProjectBox.js
+++ src/components/ProjectBox.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
    import ProjectImg1 from '../images/project_parallex-small.png'
-   import ProjectImg2 from '../images/project_parallex-small.png'
-   import ProjectImg3 from '../images/project_acme.png' 
-   import projects from '../projects'
+//    import ProjectImg2 from '../images/project_parallex-small.png'
+//    import ProjectImg3 from '../images/project_acme.png' 
+//    import projects from '../projects'
 
 
 function ProjectBox(projects) {
@@ -17,14 +17,14 @@ function ProjectBox(projects) {
                                         Read More
                                    </Link>
                               </div>
-                              <div className='project-box' key='1'>
+                              <div className='project-box' key='2'>
                                    <img className='project-box__img' src={ProjectImg1} alt='A SIMPLE WEBSITE DEMONSTRATING THE POWER OF BACKGROUND ATTACHMENT PROPERTY- BEGINNER LEVEL' />
                                    <p className='t-justify project-box__desc'>A simple website demonstrating the power of background attachment property - begineer Level</p>
                                    <Link to='/project1' className='btn__link btn__link__red' >
                                         Read More
                                    </Link>
                               </div>
-                              <div className='project-box' key='1'>
+                              <div className='project-box' key='3'>
                                    <img className='project-box__img' src={ProjectImg1} alt='A SIMPLE WEBSITE DEMONSTRATING THE POWER OF BACKGROUND ATTACHMENT PROPERTY- BEGINNER LEVEL' />
                                    <p className='t-justify project-box__desc'>A simple website demonstrating the power of background attachment property - begineer Level</p>
                                    <Link to='/project1' className='btn__link btn__link__red' >
